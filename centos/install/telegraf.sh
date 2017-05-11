ea4e4125d795e6fcbfb4cb5d5256758d9f8f1b10
Joway Wang <joway.w@gmail.com>
2017-05-11 14:44:58 +0800

    add mirror

--- centos/install/telegraf.sh
+++ centos/install/telegraf.sh
@@ -1,5 +1,7 @@
 telegraf_version=${1:-1.2.1}
 echo "installing telegraf-"$telegraf_version
-wget https://dl.influxdata.com/telegraf/releases/telegraf-$telegraf_version.x86_64.rpm
+# wget https://dl.influxdata.com/telegraf/releases/telegraf-$telegraf_version.x86_64.rpm
+wget http://static.joway.wang/mirror/telegraf-$telegraf_version.x86_64.rpm
+
 yum localinstall telegraf-$telegraf_version.x86_64.rpm
 echo "installed telegraf-"$telegraf_version
