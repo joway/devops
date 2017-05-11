e4802eb1800fc122214e1017a07623ff214d79f8
Joway Wang <joway.w@gmail.com>
2017-05-11 15:49:47 +0800

    fix bug

--- ubuntu/install/telegraf.sh
+++ ubuntu/install/telegraf.sh
@@ -3,6 +3,6 @@ echo "installing telegraf-"$telegraf_version
 # wget https://dl.influxdata.com/telegraf/releases/telegraf-$telegraf_version.x86_64.rpm
 wget http://static.joway.wang/mirror/telegraf-${telegraf_version}_amd64.deb
 
-dpkg -i telegraf_${telegraf_version}_amd64.deb
+dpkg -i telegraf-${telegraf_version}_amd64.deb
 echo "installed telegraf-"$telegraf_version
-rm telegraf_${telegraf_version}_amd64.deb
+rm telegraf-${telegraf_version}_amd64.deb
