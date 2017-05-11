744f0463ea0e016dd27f37ebcf615e579455931d
Joway Wang <joway.w@gmail.com>
2017-05-11 15:27:35 +0800

    add ubuntu telegraf

--- ubuntu/install/telegraf.sh
+++ ubuntu/install/telegraf.sh
@@ -1,8 +1,8 @@
 telegraf_version=${1:-1.2.1}
 echo "installing telegraf-"$telegraf_version
 # wget https://dl.influxdata.com/telegraf/releases/telegraf-$telegraf_version.x86_64.rpm
-wget http://static.joway.wang/mirror/telegraf-$telegraf_version.x86_64.rpm
+wget http://static.joway.wang/mirror/telegraf-${telegraf_version}_amd64.deb
 
-yum localinstall -y telegraf-$telegraf_version.x86_64.rpm
+dpkg -i telegraf_${telegraf_version}_amd64.deb
 echo "installed telegraf-"$telegraf_version
-rm telegraf-$telegraf_version.x86_64.rpm
+rm telegraf_${telegraf_version}_amd64.deb
